983642d42d7828afdb1c7e8b6a2611fa84cf8303
alessio <alessio.cirillo.1995@gmail.com>
2020-10-22 15:58:41 +0200

    pari e dispari

--- js/main.js
+++ js/main.js
@@ -26,3 +26,46 @@ if ( parola === parolaInvertita){
 } else{
     console.log('non palindroma');
 }
+
+
+
+
+//RICHIESTA PARI O DISPARI 
+var pariDispari = prompt('Pari o Dispari?')
+console.log('pari o dispari? ',pariDispari);
+
+//SCELTA NUMERO DA 1 A 5
+var sceltaNumero = parseInt( prompt('inserisci un numero da 1 a 5'));
+console.log('scelta numero ',sceltaNumero);
+
+//NUMERO RANDOM COMPUTER
+var genratoPc = numeroRandom();
+console.log('random: ',genratoPc);
+
+function numeroRandom (){
+    var numeroPc = Math.floor( Math.random () * 5) +1 ;
+
+    return numeroPc;
+}
+
+//FUNZIONE PER STABILIRE SE PARI O DISPARI
+var risultato = sommaNumeri(sceltaNumero, genratoPc);
+console.log('somma: ',risultato);
+
+function sommaNumeri (num1, num2){
+    
+    var somma = num1 + num2;
+
+    if (somma % 2 == 0){
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
+
+//DICHIARIZIONE VINCITORE
+if ( pariDispari === risultato){
+    console.log('ha vinto l\'utente');
+} else {
+    console.log('ha vinto il computer');
+}
